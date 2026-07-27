6389a80e7db02f18e04fe5b728f8a1b760927019
Kaiwen Wang <111130181+Kaiwen-W@users.noreply.github.com>
2026-07-27 21:11:20 +0100

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        perm = [0] * 26
+        for c in s1:
+            perm[ord(c) - ord('a')] += 1
+        
+        # setup sliding window
+        k = len(s1)
+        l = 0
+        freq = [0] * 26
+
+        for r in range(len(s2)):
+            char = s2[r]
+
+            freq[ord(char) - ord('a')] += 1
+
+            if sum(freq) == k:
+                if freq == perm:
+                    return True
+                
+                freq[ord(s2[l]) - ord('a')] -= 1
+                l += 1 
+
+        return False
+                
+
